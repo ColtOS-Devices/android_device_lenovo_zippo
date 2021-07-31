c4cbda1da23a44dc927d761cc248ea8b6e606cf8
Einar Gednochsson <pete80pro.pb@gmail.com>
2021-07-31 14:16:35 +0200

    zippo: Adapt for ColtOS Official

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_zippo.mk
+    $(LOCAL_DIR)/colt_zippo.mk
--- colt_zippo.mk
+++ colt_zippo.mk
@@ -6,19 +6,19 @@
 
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common Colt stuff.
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-XTENDED_BUILD_TYPE := OFFICIAL
-XTENDED_BUILD_MAINTAINER := EinarGednochsson 
 EXTRA_FOD_ANIMATIONS := true
+COLT_BUILD_TYPE=Official
+COLT_BUILD_MAINTAINER="EinarGednochsson"
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
 PRODUCT_DEVICE := zippo
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo L78051
-PRODUCT_NAME := xtended_zippo
+PRODUCT_NAME := colt_zippo
 
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
 
